2e95879b4369f08a02023081ae52f10153444d15
Kikii95 <147034133+Kikii95@users.noreply.github.com>
2023-11-21 15:09:14 +0100

    PResque  final version

--- pierre-feuille-ciseaux.py
+++ pierre-feuille-ciseaux.py
@@ -1,8 +1,8 @@
 import random
 
-choices_mapping = {1: "la Pierre", 2: "la Feuille", 3: "le Ciseaux"}
+choice_nb = {1: "la Pierre", 2: "la Feuille", 3: "le Ciseaux"}
 
-def get_user_choice():
+def get_player_choice():
     while True:
         try:
             choice = int(input("Choisissez entre Pierre (1), Feuille (2) ou Ciseaux (3) : "))
@@ -13,36 +13,51 @@ def get_user_choice():
         except ValueError:
             print("Veuillez entrer un nombre entier.")
 
-def get_computer_choice():
+def get_pc_choice():
     return random.randint(1, 3)
 
-def determine_winner(user_choice, computer_choice):
-    if user_choice == computer_choice:
-        return f"Égalité ! Vous avez tous les deux choisi {choices_mapping[user_choice]}."
-    elif (user_choice == 1 and computer_choice == 3) or \
-         (user_choice == 2 and computer_choice == 1) or \
-         (user_choice == 3 and computer_choice == 2):
-        return f"Vous avez gagné ! {choices_mapping[user_choice]} bat {choices_mapping[computer_choice]}."
+def winner(player_choice, pc_choice):
+    if player_choice == pc_choice:
+        return f"Égalité ! Vous avez tous les deux choisi {choice_nb[player_choice]}."
+    elif (player_choice == 1 and pc_choice == 3) or \
+         (player_choice == 2 and pc_choice == 1) or \
+         (player_choice == 3 and pc_choice == 2):
+        return f"Vous avez gagné ! {choice_nb[player_choice]} bat {choice_nb[pc_choice]}."
     else:
-        return f"L'ordinateur a gagné ! {choices_mapping[computer_choice]} bat {choices_mapping[user_choice]}."
+        return f"L'ordinateur a gagné ! {choice_nb[pc_choice]} bat {choice_nb[player_choice]}."
 
 def main():
-    print("Bienvenue dans le jeu Pierre-Papier-Ciseaux !")
+    print("---------------------☺---------------------")
+    print("Bienvenue sur ce jeu de Pierre, Feuille, Ciseaux !")
+    
+    reponse = input("Souhaitez-vous consulter les règles (Y/N) ? ")
+    if reponse.upper() == "Y":
+        print("---------------------☺---------------------")
+        print("Règles :")
+        print("- Le but du jeu est de vaincre l'ordinateur. - ")
+        print("------> - La partie se joue en 1 manche !")
+        print("------> - Vous avez le choix entre Pierre, Feuille, Ciseaux,")
+        print("------> - Une fois votre choix fait, l'ordinateur fait son choix aléatoirement,")
+        print("------> - Ensuite, l'affrontement : La Pierre bat le Ciseaux, le Ciseaux bat la Feuille, et la Feuille bat la Pierre.")
+        print("---------------------☺---------------------")
     
     while True:
-        user_choice = get_user_choice()
-        computer_choice = get_computer_choice()
+        player_choice = get_player_choice()
+        pc_choice = get_pc_choice()
+        
+        print("---------------------☺---------------------")
+        print(f"Vous avez choisi {choice_nb[player_choice]}")
+        print(f"L'ordinateur a choisi {choice_nb[pc_choice]}")
 
-        print(f"Vous avez choisi {choices_mapping[user_choice]}")
-        print(f"L'ordinateur a choisi {choices_mapping[computer_choice]}")
-
-        result = determine_winner(user_choice, computer_choice)
+        result = winner(player_choice, pc_choice)
+        print("---------------------☺---------------------")
         print(result)
+        print("---------------------☺---------------------")
 
         play_again = input("Voulez-vous jouer à nouveau ? (Y/N) : ")
-        if play_again.lower() != "N":
-            print("Merci d'avoir joué. Au revoir !")
+        if play_again.upper() != "Y":
+            print ("Merci d'avoir joué ! A bientôt :)")
+            print("---------------------☺---------------------")
             break
 
-if __name__ == "__main__":
-    main()
+main()
