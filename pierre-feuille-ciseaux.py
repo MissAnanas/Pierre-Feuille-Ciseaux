2c8da136e8c5c29c270f65ce09e3e84501ca609c
Anaïs 3 <pintoanais69@gmail.com>
2023-11-22 09:22:18 +0100

    comments

--- pierre-feuille-ciseaux.py
+++ pierre-feuille-ciseaux.py
@@ -1,6 +1,9 @@
 import random
 
+# Dictionnaire pour les choix possibles et leur numéro associé
 choice_nb = {1: "la Pierre", 2: "la Feuille", 3: "le Ciseaux"}
+
+# Dictionnaire pour les résultats possibles du jeu
 results = {
     (1, 1): "Égalité ! Vous avez tous les deux choisi la Pierre.",
     (2, 2): "Égalité ! Vous avez tous les deux choisi la Feuille.",
@@ -13,10 +16,11 @@ results = {
     (2, 3): "Vous avez perdu ! La Feuille bat le Ciseaux."
 }
 
-def ask_int(nb, min_val, max_val):
+# Fonction pour vérifier si un nombre est entier dans une plage donnée
+def ask_int(prompt, min_val, max_val):
     while True:
         try:
-            value = int(input(nb))
+            value = int(input(prompt))
             if min_val <= value <= max_val:
                 return value
             else:
@@ -24,19 +28,23 @@ def ask_int(nb, min_val, max_val):
         except ValueError:
             print("Veuillez entrer un nombre entier.")
 
+# Fonction pour obtenir le choix de l'ordinateur (aléatoire)
 def get_pc_choice():
     return random.randint(1, 3)
 
+# Fonction pour déterminer le gagnant
 def winner(player_choice, pc_choice):
     result_winner = (player_choice, pc_choice)
     return results.get(result_winner, "Choix invalides.")
 
+# Fonction principale du jeu
 def main():
     print("---------------------☺---------------------")
     print("Bienvenue sur ce jeu de Pierre, Feuille, Ciseaux !")
     
     reponse = input("Souhaitez-vous consulter les règles (Y/N) ? ")
     if reponse.upper() == "Y":
+        # Affichage les règles du jeu
         print("---------------------☺---------------------")
         print("Règles :")
         print("- Le but du jeu est de vaincre l'ordinateur. - ")
@@ -47,16 +55,17 @@ def main():
     
     while True:
         print("---------------------☺---------------------")
+        # Demande du choix du joueur
         player_choice = ask_int("Choisissez entre Pierre (1), Feuille (2) ou Ciseaux (3) : ", 1, 3)
-        pc_choice = get_pc_choice()
+        pc_choice = get_pc_choice()  # Obtenir le choix de l'ordinateur
         
         print("---------------------☺---------------------")
         print(f"Vous avez choisi {choice_nb[player_choice]}")
         print(f"L'ordinateur a choisi {choice_nb[pc_choice]}")
 
-        result = winner(player_choice, pc_choice)
+        result = winner(player_choice, pc_choice)  # Détermination du résultat
         print("---------------------☺---------------------")
-        print(result)
+        print(result)  # Affichage du résultat de la manche
         print("---------------------☺---------------------")
 
         play_again = input("Voulez-vous jouer à nouveau ? (Y/N) : ")
@@ -65,4 +74,5 @@ def main():
             print("---------------------☺---------------------")
             break
 
+# Appeler la fonction principale pour lancer le jeu
 main()
